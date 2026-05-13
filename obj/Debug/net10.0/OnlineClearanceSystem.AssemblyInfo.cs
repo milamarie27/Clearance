bf0ce7d18e22e9cb03ab41b83d6b13b9b7ac96ac
Mila Marie Villamor <milamarievillamor@gmail.com>
2026-05-13 17:55:52 +0800

    huhu

--- obj/Debug/net10.0/OnlineClearanceSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineClearanceSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineClearanceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baecdaa8910f35f5f33c5ba057ca1adeafa224c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8325fa0706963faac9ef96f1be3f952b89330b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineClearanceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineClearanceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
